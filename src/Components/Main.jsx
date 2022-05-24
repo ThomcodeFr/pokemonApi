8db541da95b7f8ac196e9aeaa52229aa22d185dd
Thomas Milton <perso.milton@gmail.com>
2022-05-24 14:38:33 +0200

    preparation of the development environment + configuration of main and app.jsx

---
+++ src/Components/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react"; //pas obligatoire avec creat-react-app
+
+// utilisation d'une fonction flechée
+const Main = () => {
+  return (
+    <div className="container">
+      <div className="left-content">
+
+      </div>
+      <div className="right-content">
+
+      </div>
+    </div>
+
+  )
+}
+
+export default Main;
